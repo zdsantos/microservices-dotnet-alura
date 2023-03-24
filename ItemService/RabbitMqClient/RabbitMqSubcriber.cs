a2569cf26e3531f28a5a2256dd19c11d4dfe106e
Zedequias Santos <zedequias@chatbotmaker.io>
2023-03-24 19:00:49 -0300

    add rabbitmq

---
+++ ItemService/RabbitMqClient/RabbitMqSubcriber.cs
@@ -0,0 +1,49 @@
+﻿using ItemService.EventProcessor;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ItemService.RabbitMqClient
+{
+    public class RabbitMqSubcriber : BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _nomeDaFila;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private IProcessaEvento _processaEvent;
+
+        public RabbitMqSubcriber(IConfiguration configuration, IProcessaEvento processaEvent)
+        {
+            _configuration = configuration;
+            _connection = new ConnectionFactory()
+            {
+                HostName = _configuration["RabbitMQHost"],
+                Port = int.Parse(_configuration["RabbitMQPort"]),
+            }.CreateConnection();
+
+            _channel = _connection.CreateModel();
+            _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+            _nomeDaFila = _channel.QueueDeclare().QueueName;
+            _channel.QueueBind(_nomeDaFila, exchange: "trigger", routingKey: "");
+            _processaEvent = processaEvent;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var consumidor = new EventingBasicConsumer(_channel);
+
+            consumidor.Received += (ModuleHandle, ea) =>
+            {
+                var body = ea.Body;
+
+                var mensagem = Encoding.UTF8.GetString(body.ToArray());
+                _processaEvent.Processa(mensagem);
+            };
+
+            _channel.BasicConsume(queue: _nomeDaFila, autoAck: true, consumer: consumidor);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ RestauranteService/RabbitMqClient/RabbitMqClient.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RestauranteService.Dtos;
+using System.Text;
+using System.Text.Json;
+
+namespace RestauranteService.RabbitMqClient
+{
+    public class RabbitMqClient : IRabbitMqClient
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public RabbitMqClient(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connection = new ConnectionFactory()
+            {
+                HostName = configuration["RabbitMQHost"],
+                Port = int.Parse(configuration["RabbitMQPort"])
+            }.CreateConnection();
+
+            _channel = _connection.CreateModel();
+            _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+        }
+
+        public void PublicaRestaurante(RestauranteReadDto restauranteReadDto)
+        {
+            var mensagem = JsonSerializer.Serialize(restauranteReadDto);
+            var body = Encoding.UTF8.GetBytes(mensagem);
+
+            _channel.BasicPublish(
+                exchange: "trigger",
+                routingKey: "",
+                basicProperties: null,
+                body: body
+                );
+        }
+    }
+}
